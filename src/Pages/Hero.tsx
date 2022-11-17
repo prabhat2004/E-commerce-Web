b602ed0cd56aaf103b251a1f0bc8978785bd1ed2
Dev.Prabhat <76727816+Prabhat2373@users.noreply.github.com>
2022-11-17 21:48:14 +0530

    Adding UI components

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -1,3 +1,7 @@
+import CategoryFilterIndex from "../components/Products/CategoryFilterIndex"
+import SearchBar from "../components/SearchBar"
+import Carousel from './../components/Home/Carousel';
+
 /*
   This example requires some changes to your config:
   
@@ -13,36 +17,37 @@
   ```
 */
 const callouts = [
-    {
-      name: 'Desk and Office',
-      description: 'Work from home accessories',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-      imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-      href: '#',
-    },
-    {
-      name: 'Self-Improvement',
-      description: 'Journals and note-taking',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
-      imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
-      href: '#',
-    },
-    {
-      name: 'Travel',
-      description: 'Daily commute essentials',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-      imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-      href: '#',
-    },
-  ]
-  
-  export default function Hero() {
-    return (
+  {
+    name: 'Desk and Office',
+    description: 'Work from home accessories',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: '#',
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: '#',
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: '#',
+  },
+]
+
+export default function Hero() {
+  return (
+    <>
+      <Carousel />
       <div className="bg-gray-100">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-8">
-            <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
-  
+            <h2 className="text-2xl font-bold text-gray-900">Top Collections</h2>
             <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
               {callouts.map((callout) => (
                 <div key={callout.name} className="group relative">
@@ -66,6 +71,6 @@ const callouts = [
           </div>
         </div>
       </div>
-    )
-  }
-  
+    </>
+  )
+}
---
+++ src/components/Home/Carousel.tsx
@@ -0,0 +1,42 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "../../App.css";
+
+// import required modules
+import { Pagination } from "swiper";
+
+export default function Carousel() {
+    return (
+        <>
+            <Swiper
+                pagination={{
+                    dynamicBullets: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img src={require("../../Assets/images/istockphoto-1175355990-612x612.jpg")} alt="" className="h-[70vh] " style={{ height: "70vh" }} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={require("../../Assets/images/istockphoto-1175355990-612x612.jpg")} alt="" className="h-[70vh]" style={{ height: "70vh" }} />
+                </SwiperSlide> 
+                <SwiperSlide>
+                    <img src={require("../../Assets/images/istockphoto-1175355990-612x612.jpg")} alt="" className="h-[70vh]" style={{ height: "70vh" }} />
+                </SwiperSlide>
+                 <SwiperSlide>
+                    <img src={require("../../Assets/images/istockphoto-1175355990-612x612.jpg")} alt="" className="h-[70vh]" style={{ height: "70vh" }} />
+                </SwiperSlide>
+                 <SwiperSlide>
+                    <img src={require("../../Assets/images/istockphoto-1175355990-612x612.jpg")} alt="" className="h-[70vh]" style={{ height: "70vh" }} />
+                </SwiperSlide>
+
+            </Swiper>
+        </>
+    );
+}
